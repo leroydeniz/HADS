c663fea93230fb65040260b1f13eda2aac6fde0c
Leroy Deniz <leroydeniz@icloud.com>
2021-03-06 16:56:42 +0100

    Prueba de carga

--- GUI/Profesor/inicioProfesor.aspx.vb
+++ GUI/Profesor/inicioProfesor.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        MsgBox(Session.Contents("usuario"))
+
     End Sub
 
 End Class
